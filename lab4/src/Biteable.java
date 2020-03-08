bc7e4667a8a9e31ec738dea3d22eb0dbab3e6ec5
Frr-fox <58375442+Frr-fox@users.noreply.github.com>
2020-03-08 15:58:30 +0300

    Удаление проекта

--- lab4/src/Biteable.java
+++
@@ -1,3 +0,0 @@
-public interface Biteable {
-    void bite(Clothes clothes);
-}
--- lab4/src/PoliceException.java
+++
@@ -1,6 +0,0 @@
-public class PoliceException extends Exception{
-    PoliceException(Police police){
-        super(police.getName() + " попал в тюрячку. Не повезло парню, не фортануло... Ладно, у нас они неприкосновенны, " +
-                "их нельзя закидывать в тюрячку!");
-    }
-}
